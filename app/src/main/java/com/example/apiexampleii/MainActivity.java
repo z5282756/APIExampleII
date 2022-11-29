3d33c3e32cea147de7988ccc75d12fcd878dc173
Vanshika Gupta <79128137+z5282756@users.noreply.github.com>
2022-11-30 02:27:00 +1100

    6. added attributes in MainActivity + find the attributes and connect them to XML + set up onClickListener for when button is clicked to generate text "random joke" + fixed gradle issue

--- app/src/main/java/com/example/apiexampleii/MainActivity.java
+++ app/src/main/java/com/example/apiexampleii/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.apiexampleii;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity { 
+    // add attributes
+    Button mButton;
+    TextView mJoke;
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // find the attributes and connect to XML (find the handle to the UI elements):
+        mButton = findViewById(R.id.btnDisplayJoke);
+        mJoke = findViewById(R.id.tvJoke);
+
+        //when click on button...
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mJoke.setText("random joke");
+            }
+        });
     }
 }
