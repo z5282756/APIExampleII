4e3c47110d64f00afaed31730bc47ecf73856ab8
Vanshika Gupta <79128137+z5282756@users.noreply.github.com>
2022-11-30 01:27:27 +1100

    2. Create new java class in package "API" called "Joke" and paste the json converted class + getters/setters...
    (got API from online source, used Hopscotch GET to make into JSON file, then used json to pojo to convert into gson to paste here)

---
+++ app/src/main/java/com/example/apiexampleii/API/Joke.java
@@ -0,0 +1,89 @@
+package com.example.apiexampleii.API;
+
+import java.util.List;
+import javax.annotation.Generated;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+@Generated("jsonschema2pojo")
+public class Joke {
+
+    @SerializedName("categories")
+    @Expose
+    private List<Object> categories = null;
+    @SerializedName("created_at")
+    @Expose
+    private String createdAt;
+    @SerializedName("icon_url")
+    @Expose
+    private String iconUrl;
+    @SerializedName("id")
+    @Expose
+    private String id;
+    @SerializedName("updated_at")
+    @Expose
+    private String updatedAt;
+    @SerializedName("url")
+    @Expose
+    private String url;
+    @SerializedName("value")
+    @Expose
+    private String value;
+
+    public List<Object> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<Object> categories) {
+        this.categories = categories;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getIconUrl() {
+        return iconUrl;
+    }
+
+    public void setIconUrl(String iconUrl) {
+        this.iconUrl = iconUrl;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+}
