98b2205b67b8c6a7d34cf5c7440edc0571f2f089
Vanshika Gupta <79128137+z5282756@users.noreply.github.com>
2022-11-30 02:47:55 +1100

    8. in MainActivity we   a) instantiated an object from retrofit class   b) instatiated an object from JokeService class   c) linked the retrofit object created (retrofit) to the instance of the service class (service)   d) added .enqueue --> makes API call and runns it through background threading for us

--- app/src/main/java/com/example/apiexampleii/MainActivity.java
+++ app/src/main/java/com/example/apiexampleii/MainActivity.java
@@ -3,14 +3,23 @@ package com.example.apiexampleii;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.apiexampleii.API.Joke;
+import com.example.apiexampleii.API.JokeService;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class MainActivity extends AppCompatActivity { 
+public class MainActivity extends AppCompatActivity {
+    String TAG = "MainActivity";
+
     // add attributes
     Button mButton;
     TextView mJoke;
@@ -25,21 +34,39 @@ public class MainActivity extends AppCompatActivity {
         mJoke = findViewById(R.id.tvJoke);
 
         // set up retrofit to make API call
-        // set up object from retrofit class and specify base URL and add
+        // instantiate object from retrofit class and specify base URL and add
         // gson as the converterFactory:
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://api.chucknorris.io/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
-
+        // instantiate an object from the JokeService class we added:
+        JokeService service = retrofit.create(JokeService.class);
+        // to be able to use that, we need to have implementation for the method we just added:
 
 
         //when click on button...
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mJoke.setText("random joke");
+
+                Call<Joke> responseCall = service.getRandomJoke();
+                responseCall.enqueue(new Callback<Joke>() {
+                    @Override
+                    public void onResponse(Call<Joke> call, Response<Joke> response) {
+                        Log.d(TAG, "API success");
+                        Joke joke = response.body();
+                        mJoke.setText(joke.getValue());
+
+                    }
+
+                    @Override
+                    public void onFailure(Call<Joke> call, Throwable t) {
+                        Log.d(TAG, "API failure");
+
+                    }
+                });
             }
         });
     }
