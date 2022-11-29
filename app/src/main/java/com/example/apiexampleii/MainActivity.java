27ea529d12a9df839d40f079304a5eabeb937fec
Vanshika Gupta <79128137+z5282756@users.noreply.github.com>
2022-11-30 02:37:17 +1100

    7. set up retrofit in MainActivity to make API call + set up object from retrofit class and specify base URL and add + gson as the converterFactory:

--- app/src/main/java/com/example/apiexampleii/MainActivity.java
+++ app/src/main/java/com/example/apiexampleii/MainActivity.java
@@ -7,6 +7,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class MainActivity extends AppCompatActivity { 
     // add attributes
     Button mButton;
@@ -20,6 +23,17 @@ public class MainActivity extends AppCompatActivity {
         // find the attributes and connect to XML (find the handle to the UI elements):
         mButton = findViewById(R.id.btnDisplayJoke);
         mJoke = findViewById(R.id.tvJoke);
+
+        // set up retrofit to make API call
+        // set up object from retrofit class and specify base URL and add
+        // gson as the converterFactory:
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://api.chucknorris.io/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+
+
 
         //when click on button...
         mButton.setOnClickListener(new View.OnClickListener() {
